3772fbb1bc148f710230d62482ea1494c6978ab2
nick <gruzinov15@yandex.ru>
2021-01-07 02:28:31 +0300

    The parser is ready
    Moved the convertion & plot-checking piece from test_parsed_data.py to
    the main script, added a couple of small improvements.
    The next step is applying the parser to all the 19 videos.

--- video_parser_prototype.py
+++ video_parser_prototype.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 data_file_path = 'prototype_raw_data.csv'
 video_file_path = 'videos/VID_20201127_135945.mp4'
-
 # Notch rectangle coordinates
 x1, y1, x2, y2 = 280, 300, 285, 330
 
@@ -18,25 +17,17 @@ nframes = []
 
 print('You will be prompted for which notch you see in the rectangle.\n'
       'Type an integer. Anything else would mean the notch was detected\n'
-      'incorrectly.')
+      'incorrectly. Type drop to discard the last input.\n')
 
 while vidcap.isOpened():
     success, colored_frame = vidcap.read()
     if not success:
-        print("Can't receive frame (stream end?). Exiting.")
+        print('Could not receive frame (stream end?). Exiting.')
         break
     curr_nframe += 1  # starting from 1
 
     frame = cv2.cvtColor(colored_frame, cv2.COLOR_BGR2GRAY)
-
     cv2.rectangle(colored_frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-    if cv2.waitKey(1) == ord(' '):  # pause
-        time.sleep(0.5)
-
-    if cv2.waitKey(1) == ord('q'):  # quit
-        print(curr_nframe)
-        break
 
     # Convert image into monochrome
     frame = cv2.cvtColor(colored_frame, cv2.COLOR_BGR2GRAY)
@@ -47,18 +38,47 @@ while vidcap.isOpened():
 
     if np.mean(frame[y1:y2, x1:x2]) == 255:
         cv2.imshow('frames with notches', colored_frame[100:600, 100:600])
+
         manual_degree = input('> ')
+        if manual_degree.count('drop') > 0 and angles and nframes:
+            nframes.pop()
+            angles.pop()
+
         try:
             manual_degree = int(manual_degree)
         except ValueError:  # notch was detected incorrectly
             continue
-        angles.append(int(manual_degree))
-        nframes.append(curr_nframe)
+
+        if 0 <= manual_degree <= 360:
+            nframes.append(curr_nframe)
+            angles.append(manual_degree)
+        else:
+            print('Input out of bounds, be careful.')
+
+    cv2.waitKey(1)
+
+vidcap.release()
+cv2.destroyAllWindows()
+
+# Count full turns, add 360 degrees for each one.
+# Assumes 1) the values strictly increase 2) no full turn was skipped.
+# Otherwise we're in trouble.
+turn = 0
+for i in range(1, len(angles)):
+    angles[i] += 360 * turn
+    if angles[i] < angles[i - 1]:
+        turn += 1
+        angles[i] += 360
+
+# Convert to physically true units
+angles = list(map(lambda angle: (angle - angles[0]) / 180 * np.pi, angles))
+times = list(map(lambda nframe: (nframe - nframes[0]) / fps, nframes))
+
+# Save the plot to temporary file to check the results visually
+plt.scatter(times, angles)
+plt.savefig('figures/tmp_plot.png')
 
 # Write the data to the csv file
 with open(data_file_path, 'w') as file:
    for i in range(len(angles)):
-       file.write(f'{nframes[i]},{angles[i]}\n')
-
-vidcap.release()
-cv2.destroyAllWindows()
+       file.write(f'{times[i]},{angles[i]}\n')
