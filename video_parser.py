d8c7dedcdbc8da7c98ac0ece5692276fda486cc4
nick <gruzinov15@yandex.ru>
2021-01-09 01:05:02 +0300

    Parse video 17
    Strange results.

--- video_parser.py
+++ video_parser.py
@@ -3,8 +3,8 @@ import cv2
 import time
 import matplotlib.pyplot as plt
 
-data_file_path = 'csv_data/16.csv'
-video_file_path = 'videos/VID_20201127_150515.mp4'
+data_file_path = 'csv_data/17.csv'
+video_file_path = 'videos/VID_20201127_150804.mp4'
 # Notch rectangle coordinates
 x1, y1, x2, y2 = 280, 485, 285, 520
 
